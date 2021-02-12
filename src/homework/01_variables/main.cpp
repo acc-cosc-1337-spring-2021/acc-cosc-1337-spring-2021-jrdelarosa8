d13133fbb3ba7ba7f2a36e2ef480df83b44f8106
Jose De La Rosa <jrdelarosa8@gmail.com>
2021-02-11 20:27:14 -0600

    COSC-1337: Submission of homework 2

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,42 @@
-﻿//write include statements
+﻿#include "variables.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::string;
 
-//write namespace using statement for cout
-
-
-/*
-Call multiply_numbers with 10 and 10 parameter values and display function result
-*/
 int main()
 {
+	double meal_amount, tax_amount, tip_amount, tip_rate, total;
+	string divider = "-----------------------";
+
+	cout << "Enter the cost of the meal: ";
+	cin >> meal_amount;
+
+	cout << "Enter the tip rate (as a decimal): ";
+	cin >> tip_rate;
 	
+	tax_amount = get_sales_tax(meal_amount);
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	total = meal_amount + tax_amount + tip_amount;
+	
+	cout << endl << setw(23) << "Meal Receipt" << endl;
+	cout << divider << endl;
+	cout << setprecision(2) << fixed;
+
+	cout << setw(15) << "Meal Amount: " << setw(8) << meal_amount << endl;
+	cout << setw(15) << "Sales Tax: " << setw(8) << tax_amount << endl;
+	cout << setw(15) << "Tip Amount: " << setw(8) << tip_amount << endl;
+
+	cout << divider << endl;
+
+	cout << setw(15) << "Total: " << setw(8) << total << endl;
 
 	return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -1,14 +1,13 @@
 #include "variables.h"
 
-//example
-int add_numbers(int num1, int num2)
+const double TAX_RATE = 0.0675;
+
+double get_sales_tax(double meal_amount)
 {
-	return num1 + num2;
+	return meal_amount * TAX_RATE;
 }
 
-//write code for multiply_numbers function here
-//function returns product of num1 and num2
-
-
-
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
