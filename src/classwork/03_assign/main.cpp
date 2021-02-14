75aa98703d92cb309e5589add066d37018adf3c4
Jose De La Rosa <jrdelarosa8@gmail.com>
2021-02-14 12:04:43 -0600

    COSC-1337: Submission of homework 3

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,24 @@
-//Write the include statement for decisions.h here
+#include <iostream>
+#include <decision.h>
 
-//Write namespace using statements for cout and cin
+using std::cin; using std::cout; using std::endl;
 
 int main() 
 {
+	int grade;
+
+	cout << "Enter a number grade between 0 and 100: ";
+	cin >> grade;
+	
+	if (grade < 0 || grade > 100)
+	{
+		cout << "The grade you have entered is out of range.";
+	}
+	else
+	{
+		cout << "(If) - You have earned an " << get_letter_grade_using_if(grade) << endl;
+		cout << "(Switch) - You have earned an " << get_letter_grade_using_switch(grade) << endl;
+	}
 
 	return 0;
 }
-
